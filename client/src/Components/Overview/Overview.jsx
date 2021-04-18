408469730f72b16e1f1c505ddb72ed08ac9bc3b2
Josh Lee <leeyjosh@gmail.com>
2021-04-18 04:59:52 -0400

    Create overview component

---
+++ client/src/Components/Overview/Overview.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Overview extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="overview">Overview</div>
+    );
+  }
+}
+
+export default Overview;
