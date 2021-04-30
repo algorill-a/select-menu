91ea6f5cc55df1b1e3c7608e22a6cdcdb9b1a43e
Mark Valdez <mbqvaldez@gmail.com>
2021-04-29 20:18:15 -0700

    fix bars

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,5 +33,13 @@ module.exports = {
         some: ['nesting', 'id'],
       },
     }],
+    'no-console': 'off',
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+        message: 'Unexpected property on console object was called',
+      },
+    ],
   },
 };
