67ac0e00d8cea3903962fa8ff343aa841dc1fa56
Mark Valdez <mbqvaldez@gmail.com>
2021-05-01 15:10:36 -0700

    update webpack and readME

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const SRC_DIR = path.join(__dirname, './client/src');
 const OUT_DIR = path.join(__dirname, './client/dist');
 
 module.exports = {
-  mode: 'development',
+  mode: 'performance',
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
